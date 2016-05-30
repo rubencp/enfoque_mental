5fd10bf732bbf34245180e695c6ee9966e46ba29
Rubén Cervantes Ponce <ruben.cervantes@gmail.com>
2016-05-30 22:21:16 +0200

    Start Writing Gherkin

---
+++ features/step_definitions/starting_steps.rb
@@ -0,0 +1,7 @@
+Given(/^Application starts$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the route is valid to receive mesages from twilio$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
