7ed082411088bc4144a79eb468494fbe949f8d6d
Rubén Cervantes Ponce <ruben.cervantes@gmail.com>
2016-06-22 23:47:13 +0200

    make a test 2+2

---
+++ spec/controllers/receive_messages_controller_spec.rb
@@ -0,0 +1,23 @@
+
+
+describe "POST #receive" do
+  it "receives an SMS from Twilio" do
+      
+    3.should eq(2)
+  end
+  
+  it "gets the conversation from the message" do
+
+  end
+
+  it "extracts the phone number" do
+    5.should eq(3)
+  end
+  
+  it "identifies next message to send" do
+  end
+
+  it "sends new message back to sender" do
+  end
+
+end
