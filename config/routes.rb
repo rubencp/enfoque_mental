38db08074f6c8ca1b0fda1cf399993add1174e5c
Rubén Cervantes Ponce <ruben.cervantes@gmail.com>
2016-07-09 08:57:25 +0200

    add GET method

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   post 'receive_message', to: 'receive_message#receive'
+  get 'receive_message', to: 'receive_message#receive'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
