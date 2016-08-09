c8099d8d8c57439ace6088e8729502d0a3792f97
Javier Cervantes <javier@sawyereffect.com>
2016-08-09 18:18:33 -0400

    Remove twiml conversion as not needed?

--- app/controllers/receive_message_controller.rb
+++ app/controllers/receive_message_controller.rb
@@ -1,14 +1,7 @@
 class ReceiveMessageController < ApplicationController
   def receive
-
-   from_number = params["From"]
-   message = Conversation.new(from_number).next
-  
-
-   twiml = Twilio::TwiML::Response.new do |r|
-    r.Message message 
-   end
-   twiml.text
+    from_number = params['From']
+    message = Conversation.new(from_number).next
 
     render plain: message
   end
