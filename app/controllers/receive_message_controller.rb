d5fae630df70ad5e7f4a58b6a361bb79b628cdba
Rubén Cervantes Ponce <ruben.cervantes@gmail.com>
2016-08-11 17:18:33 +0200

    regresando cambios que según yo si se necesitan
    la db de producción es postgres no default = sqlite
    TwiML es las union que hace la magia de mandar el mensaje.

--- app/controllers/receive_message_controller.rb
+++ app/controllers/receive_message_controller.rb
@@ -3,6 +3,11 @@ class ReceiveMessageController < ApplicationController
     from_number = params['From']
     message = Conversation.new(from_number).next
 
+   twiml = Twilio::TwiML::Response.new do |r|
+    r.Message message 
+   end
+   twiml.text
+
     render plain: message
   end
 end
