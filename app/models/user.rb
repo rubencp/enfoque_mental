cfa7ef1e6f8b6f9d5c094426dd558d1f5cde61f2
Rubén Cervantes Ponce <ruben.cervantes@gmail.com>
2016-09-01 07:39:32 +0200

    make test pass
    I don’t like the initialisation of messages, needs to be refactored

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
     super
     self.number = someparams[:number]
     self.message_nr = 0
-    self.conversation_nr = 0
+    self.conversation_nr = 0  
   end
 
   def increment_conversation
@@ -15,8 +15,17 @@ class User < ApplicationRecord
     end
   end
 
-  def new_conversation(phone_number)
+
+  def self.new_conversation(phone_number)
     @user = User.find_or_create_by(number: phone_number)
+    #messages
+  end
+
+  def next
+    sms_count = self.message_nr
+    self.message_nr += 1
+    self.save
+
     @messages = []
     @messages << '¿Quién eres?'
     @messages << '¿Cuál es tu propósito en esta vida?'
@@ -25,13 +34,23 @@ class User < ApplicationRecord
     @messages << '¿Qué puedes y decides plasmar como tu obra maestra?'
     @messages << '¿Qué canción te motiva a plasmar tu obra maestra?'
     @messages << '¿Qué siguiente acción te necesita?'
+
+    @messages[sms_count]
   end
 
-  def next
-    sms_count = @user.message_nr
-    @user.message_nr += 1
-    @user.save
-    @messages[sms_count]
+
+
+private
+
+  def messages
+    @messages = []
+    @messages << '¿Quién eres?'
+    @messages << '¿Cuál es tu propósito en esta vida?'
+    @messages << '¿Cuál es tu vocación?'
+    @messages << '¿Cómo te sientes?'
+    @messages << '¿Qué puedes y decides plasmar como tu obra maestra?'
+    @messages << '¿Qué canción te motiva a plasmar tu obra maestra?'
+    @messages << '¿Qué siguiente acción te necesita?'
   end
 
 
