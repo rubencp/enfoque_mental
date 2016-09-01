343388cc06f76deb5e828b8d2b935f04b8e92f37
Rubén Cervantes Ponce <ruben.cervantes@gmail.com>
2016-09-01 07:11:52 +0200

    Create test for new user before merge
    Merge models into a single one

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,25 @@ class User < ApplicationRecord
       self.conversation_nr += 1
     end
   end
+
+  def new_conversation(phone_number)
+    @user = User.find_or_create_by(number: phone_number)
+    @messages = []
+    @messages << '¿Quién eres?'
+    @messages << '¿Cuál es tu propósito en esta vida?'
+    @messages << '¿Cuál es tu vocación?'
+    @messages << '¿Cómo te sientes?'
+    @messages << '¿Qué puedes y decides plasmar como tu obra maestra?'
+    @messages << '¿Qué canción te motiva a plasmar tu obra maestra?'
+    @messages << '¿Qué siguiente acción te necesita?'
+  end
+
+  def next
+    sms_count = @user.message_nr
+    @user.message_nr += 1
+    @user.save
+    @messages[sms_count]
+  end
+
+
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe '#next' do
+    let(:from_number)  { '+4917664825233' }
+    let(:user_conversation) { User.new_conversation(from_number) }
+
+    it 'responds with the first message of the conversation' do
+      expect(user_conversation.next).to eq('¿Quién eres?')
+    end
+
+    it 'gets different message on 2nd time' do
+      user_conversation.next
+      expect(user_conversation.next).to eq('¿Cuál es tu propósito en esta vida?')
+    end
+
+    it 'responds with the first message after all messages' do
+      7.times { user_conversation.next }
+
+      expect(user_conversation.next).to eq('¿Quién eres?')
+    end
+  end
+end
