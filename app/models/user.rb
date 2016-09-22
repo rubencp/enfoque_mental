88f9bd0d0693b7a6fccc47b54e36b64e4e69b38e
rubencp <ruben.cervantes@gmail.com>
2016-09-22 15:15:00 +0200

    Revert "Backend added to debug user timestamps"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,65 +1,62 @@
 class User < ApplicationRecord
-
   MESSAGES = []
-    MESSAGES << '¿Quién eres?'
-    MESSAGES << '¿Cuál es tu propósito en esta vida?'
-    MESSAGES << '¿Cuál es tu vocación?'
-    MESSAGES << '¿Cómo te sientes?'
-    MESSAGES << '¿Qué puedes y decides plasmar como tu obra maestra?'
-    MESSAGES << '¿Qué canción te motiva a plasmar tu obra maestra?'
-    MESSAGES << '¿Qué siguiente acción te necesita?'
+  MESSAGES << '¿Quién eres?'
+  MESSAGES << '¿Cuál es tu propósito en esta vida?'
+  MESSAGES << '¿Cuál es tu vocación?'
+  MESSAGES << '¿Cómo te sientes?'
+  MESSAGES << '¿Qué decides plasmar como tu obra maestra?'
+  MESSAGES << '¿Qué canción te motiva?'
+  MESSAGES << '¿Qué siguiente acción te necesita?'
 
-
-  scope :older_than, -> (days) { where("last_message_at < ?", Date.today-days) }
-  scope :older_than_hours, -> (hours) { where("last_message_at < ?", Time.now-hours) }
-
+  scope :older_than_hours, -> (n) { where('last_message_at < ?', Time.now - n.hours) }
 
   def initialize(someparams)
     super
-    default_values
+    self.number = someparams[:number]
+    self.message_nr = 0
+    self.conversation_nr = 0
+    self.last_message_at = Time.now
   end
 
   def increment_conversation
-    self.message_nr += 1 
-    if self.message_nr >= 7 
+    self.message_nr += 1
+    if self.message_nr >= 7
       self.message_nr = 0
       self.conversation_nr += 1
     end
+    update_last_message_timestamp
+  end
+
+  def update_last_message_timestamp
     self.last_message_at = Time.now
     save
   end
 
   def self.new_conversation(phone_number)
     @user = User.find_or_create_by(number: phone_number)
-
   end
 
-  def message
+  def next
     message = MESSAGES[message_nr]
+    increment_conversation
+    message
   end
 
+  def last
+    MESSAGES[message_nr]
+  end
 
   def send_reminder_message
- 
-    # set up a client to talk to the Twilio REST API 
-    @client = Twilio::REST::Client.new ENV['TWILIO_SID'], ENV['TWILIO_AUTH'] 
-     
-    @client.account.messages.create({
-       :from => '+525549998395', 
-       :to => self.number, 
-       :body => message,  
-     }) 
+    account_sid = ENV['TWILIO_SID']
+    auth_token = ENV['TWILIO_AUTH']
 
-    puts "Re-Sending:" + message + " to: " + self.number
+    # set up a client to talk to the Twilio REST API
+    @client = Twilio::REST::Client.new account_sid, auth_token
 
+    @client.account.messages.create(from: '+525549998395',
+                                    to: number,
+                                    body: last)
+
+    puts 'Re-Sending:' + last + ' to: ' + number
   end
-
-
-  private
-    def default_values
-      self.message_nr ||= 0
-      self.conversation_nr ||= 0
-      self.last_message_at ||= Time.now
-    end
-
 end
