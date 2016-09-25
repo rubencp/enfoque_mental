fb6846cd7c83442db58224c581e6e014b12d4d27
Rubén Cervantes Ponce <ruben.cervantes@gmail.com>
2016-09-25 22:26:36 +0200

    Changing HardCoded Value to Env Variable

--- app/models/user.rb
+++ app/models/user.rb
@@ -53,7 +53,7 @@ class User < ApplicationRecord
     # set up a client to talk to the Twilio REST API
     @client = Twilio::REST::Client.new account_sid, auth_token
 
-    @client.account.messages.create(from: '+525549998395',
+    @client.account.messages.create(from: ENV['FROM_TWILIO_NUMBER'],
                                     to: number,
                                     body: last)
 
