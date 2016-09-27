4f5978f9bbde1131e603f62ce48f0c86f68fd3f0
Javier Cervantes <javier@sawyereffect.com>
2016-09-27 14:27:24 -0400

    Adding new conversation object

---
+++ app/models/conversation.rb
@@ -0,0 +1,42 @@
+class Conversation
+  MESSAGES = [
+    '¿Quién eres?',
+    '¿Cuál es tu propósito en esta vida?',
+    '¿Cuál es tu vocación?',
+    '¿Cómo te sientes?',
+    '¿Qué decides plasmar como tu obra maestra?',
+    '¿Qué canción te motiva?',
+    '¿Qué siguiente acción te necesita?'
+  ].freeze
+
+  def initialize(user)
+    @user = user
+  end
+
+  def next_message
+    update_message_nr
+    @user.last_message_at = Time.now
+    @user.save!
+
+    MESSAGES[@user.message_nr - 1]
+  end
+
+  def send_reminder
+    client  = Twilio::REST::Client.new ENV['TWILIO_SID'], ENV['TWILIO_AUTH']
+    message = next_message
+
+    client.account.messages.create(from: ENV['FROM_TWILIO_NUMBER'],
+                                   to: @user.number, body: message)
+  end
+
+  private
+
+  def update_message_nr
+    if @user.message_nr >= 6
+      @user.message_nr = 0
+      @user.conversation_nr += 1
+    else
+      @user.message_nr += 1
+    end
+  end
+end
