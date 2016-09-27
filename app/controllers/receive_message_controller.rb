fffec6331384abbae636eacd003545dd487e8b6f
Javier Cervantes <javier@sawyereffect.com>
2016-09-27 11:22:28 -0400

    fix messages

--- app/controllers/receive_message_controller.rb
+++ app/controllers/receive_message_controller.rb
@@ -2,10 +2,9 @@ class ReceiveMessageController < ApplicationController
   def receive
     from_number = params['From']
     user        = User.new(number: from_number)
-    message     = user.next_message
 
     user.increment_conversation
 
-    render plain: message
+    render plain: user.next_message
   end
 end
